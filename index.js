94f05361f3d016507dffc989403c5e168262a749
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-03 07:46:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,7 @@ function getRepositories() {
 function showCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
+    .map(c => '<li><strong>' + c.author.login + '</strong> - ' + c.commit.message + '</li>')
     .join('')}</ul>`;
   document.getElementById('commits').innerHTML = commitsList;
 }
@@ -32,9 +25,9 @@ function showCommits() {
   const commits = JSON.parse(this.responseText);
   console.log(commits);
   const commitsList = `<ul>${commits
-    .map(r => '<li>'  + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>')
+    .map(c => '<li>'  + c.name + ' - <a href="#" data-repo="' + c.name + '" onclick="getCommits(this)">Get Commits</a></li>')
     .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
+  document.getElementById('commits').innerHTML = commitsList;
 }
 
 
