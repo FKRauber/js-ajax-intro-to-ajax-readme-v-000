f2f7165ea99548662c7966d4139d067a3a81801b
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-03 07:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,11 +32,10 @@ function showCommits() {
 
 
 
-function getCommits(el) {
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
-
-  req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat' + name + '/commits');
-  req.send();
-}
+ function getCommits(el) {
+    const name = el.dataset.repo;
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', showCommits);
+    req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+    req.send();
+  }
